9049e731b675ac335bc62ff70fe7c0e240a0e855
wmhe <william.he56@gmail.com>
2024-12-20 16:33:32 -0800

    Add basic FullCalendar

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth'
+      }
+    }
+  }
+}
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <FullCalendar :options="calendarOptions" />
 </template>
